d4cd33a677dc80b83d3f195d5a60cb69c21625d4
67vivekraj <vivekraj.srivastava@linktoany.com>
2024-09-02 11:49:44 +0530

    carousel

--- src/UI/atoms/CarouselAka.jsx
+++ src/UI/atoms/CarouselAka.jsx
@@ -12,47 +12,47 @@ const CarouselAka = ({ images }) => {
   };
 
   return (
-    <div id="default-carousel" className="relative w-full" data-carousel="slide">
+    <div id="default-carousel" className="relative" data-carousel="slide">
       {/* Carousel wrapper */}
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div className="relative h-72 overflow-hidden rounded-lg md:h-96">
         {images.map((image, index) => (
           <div
             key={index}
-            className={`hidden duration-700 ease-in-out ${index === currentIndex ? 'block' : ''}`}
+            className={`duration-1000 ease-in-out ${index === currentIndex ? 'block' : 'hidden'}`}
             data-carousel-item
           >
             <img
               src={image.src}
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute block   h-72  rounded-lg  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               alt={image.alt}
             />
           </div>
         ))}
       </div>
-      {/* Slider indicators */}
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+       
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-1 rtl:space-x-reverse">
         {images.map((_, index) => (
-          <button
+          <div
             key={index}
-            type="button"
-            className={`w-3 h-3 rounded-full ${index === currentIndex ? 'bg-blue-600' : 'bg-gray-300'}`}
+             
+            className={`w-2 h-2 rounded-full ${index === currentIndex ? 'bg-blue-600' : 'bg-gray-300'}`}
             aria-current={index === currentIndex}
             aria-label={`Slide ${index + 1}`}
             onClick={() => setCurrentIndex(index)}
             data-carousel-slide-to={index}
-          ></button>
+          ></div>
         ))}
       </div>
-      {/* Slider controls */}
+      
       <button
         type="button"
         className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         onClick={handlePrev}
         data-carousel-prev
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-white/30 dark:bg-gray-200 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 h-4 text-white dark:text-gray-400 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -75,9 +75,9 @@ const CarouselAka = ({ images }) => {
         onClick={handleNext}
         data-carousel-next
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full bg-white/30 dark:bg-gray-200 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 h-4 text-white dark:text-gray-400 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
